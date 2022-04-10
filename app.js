733a3816dbb4c3ebf2d684b358e38c7545076095
Chris Wiseman <wisemanchris92@gmail.com>
2022-04-10 15:38:39 -0500

    added functionality to the delete button

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list');
 
 // event listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
 // functions
 function addTodo(event){
@@ -15,7 +16,7 @@ function addTodo(event){
     todoDiv.classList.add("todo");
     // create li
     const newTodo = document.createElement('li');
-    newTodo.innerText = 'hey';
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo); // sticking it inside of the new todoDiv, now we have a div with an li of todo-item
     // completed button, check mark button
@@ -30,4 +31,21 @@ function addTodo(event){
     todoDiv.appendChild(trashButton);
     // append to list
     todoList.appendChild(todoDiv);
+    // clear todoInput value
+    todoInput.value = "";
+}
+
+function deleteCheck(event){
+    const item = event.target;
+    // delete the todo
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+
+    // check mark
+    if(item.classList[0] === "complete-btn"){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
 }
