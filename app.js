86ed11b9b1b934769e33a7b01de19d91622c45bf
Chris Wiseman <wisemanchris92@gmail.com>
2022-04-10 12:53:32 -0500

    added addToto function to app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+// selectors
+const todoInput = document.querySelector(',todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// event listeners
+todoButton.addEventListener('click', addTodo);
+
+// functions
+function addTodo(event){
+    // prevent form from submitting
+    event.preventDefault();
+    // todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    // create list item
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo); // sticking it inside of todoDiv, now we have a div with an li of todo-item
+    // completed button, check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    // trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+    // append to list
+    todoList.appendChild(todoDiv);
+}
